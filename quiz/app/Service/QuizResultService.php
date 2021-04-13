af409a135bc42ec1889d8fc506fa2bc684f3e2ab
Nikolai Nazarov <nikolai.nazarov@simbirsoft.com>
2021-04-13 12:35:54 +0400

    init

---
+++ quiz/app/Service/QuizResultService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use App\DTO\QuizDTO;
+use App\DTO\QuestionDTO;
+use App\DTO\ChoiceDTO;
+use App\DTO\AnswersDTO;
+use App\DTO\AnswerDTO;
+
+use Exception;
+
+class QuizResultService
+{
+    private QuizDTO $quiz;
+    private AnswersDTO $answers;
+
+    public function __construct(QuizDTO $quiz, AnswersDTO $answers)
+    {
+        $this->quiz = $quiz;
+        $this->answers = $answers;
+    }
+
+    public function getResult(): float
+    {
+        // your code here
+    }
+}
